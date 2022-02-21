81635cd4df55f50be5a352c88409f5418afdec1e
TakKokubu <kokubu.tk@gmail.com>
2022-02-21 13:52:02 +0900

    create-header

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+
+export default {
+  componets: {
+    TheHeader,
+  },
+};
+</script>
