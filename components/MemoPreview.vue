da38f3cdf8574096f5184f4e22aaab520d3bbf18
TakKokubu <kokubu.tk@gmail.com>
2022-02-24 13:14:02 +0900

    show-tag-list

--- components/MemoPreview.vue
+++ components/MemoPreview.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <nuxt-link v-bind:to="`/memo/${memo.id}`">
+    <nuxt-link :to="`/memo/${memo.id}`">
       {{ memo.id }}
       {{ memo.content }}
+      <li class="tag-list" v-for="tag in memo.tags" :key="tag.id">
+        タグ-{{ tag.name }}
+      </li>
     </nuxt-link>
     <button @click="deleteMemo(memo)">削除</button>
     <button v-if="memo.is_like" @click="unFavo(memo)">お気に入り解除</button>
@@ -29,6 +32,7 @@ export default {
     addFavo(memo) {
       this.$store.dispatch("addFavo", memo.id).then(() => {
         this.$router.push("/memo");
+        console.log(memo);
       });
     },
     unFavo(memo) {
@@ -39,3 +43,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.tag-list {
+  display: inline-block;
+  padding: 10px;
+  text-decoration: none;
+}
+</style>
