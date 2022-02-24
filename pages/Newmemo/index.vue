71d9eed23783c3e89187444b4d808433e294bcb2
TakKokubu <kokubu.tk@gmail.com>
2022-02-24 16:30:49 +0900

    select-boxの追加

--- pages/Newmemo/index.vue
+++ pages/Newmemo/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <textarea v-model="content" class="memo-text"></textarea>
+    <select name="sampleName[]" class="select-multiple" multiple>
+      <option value="値Ａ">選択肢Ａ</option>
+      <option value="値Ｂ">選択肢Ｂ</option>
+      <option value="値Ｃ">選択肢Ｃ</option>
+      <option value="値Ｄ">選択肢Ｄ</option>
+      <option value="値Ｅ">選択肢Ｅ</option>
+    </select>
     <button @click="addMemo" class="memo-add-button">作成</button>
   </div>
 </template>
@@ -29,7 +36,11 @@ export default {
 }
 .memo-add-button {
   display: block;
-  margin-left: 160px;
+  margin-left: 100px;
+}
+.select-multiple {
+  margin-left: 100px;
+  display: block;
 }
 </style>
 
